13ab3987747281dadfba383fa0475c5973389858
Kochiyama <marcelokochiyama2210@gmail.com>
2020-08-07 17:34:45 -0300

    add favorites implementation, add or remove favorite proffy

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,14 +1,138 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import { ScrollView, TextInput, BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons'
+import AsyncStorage from '@react-native-community/async-storage';
+
+import api from '../../services/api';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 
 import styles from './styles';
 
 function TeacherList() {
+  const [ isFiltersVisible, setIsFiltersVisible ] = useState(false);
+  const [favorites, setFavorites] = useState<number[]>([]);
+
+  const [teachers, setTeachers] = useState([]);
+
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
+
+  function loadFavorites() {
+    AsyncStorage.getItem('favorites').then(response => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response);
+
+        
+        console.log("!!!!! favorited teacher's id !!!!!")
+        console.log(favoritedTeachers)
+        console.log("")
+
+        setFavorites(favoritedTeachers);
+      }
+    })
+  }
+
+  function handleToggleFiltersVisible() {
+    setIsFiltersVisible(!isFiltersVisible);
+  }
+
+  async function handleApplyFilters() {
+    loadFavorites();
+
+    const response = await api.get("classes", {
+      params: {
+        subject,
+        week_day,
+        time
+      }
+    })
+
+    setTeachers(response.data);
+  }
+
   return (
     <View style={styles.container}>
-      <PageHeader title="Proffys disponíveis"/>
+      <PageHeader 
+        title="Proffys disponíveis"
+        headerRight={(
+        <BorderlessButton 
+          onPress={handleToggleFiltersVisible}
+          style={styles.filterButton}
+        >
+          <Text style={styles.filterButtonText}>Filtrar</Text>
+          <Feather name="filter" size={20} color="#fff"/>
+        </BorderlessButton>
+      )}
+      >
+        { isFiltersVisible && (
+          <View style={styles.searchForm}>
+            <Text style={styles.label}>Matéria</Text>
+
+            <TextInput
+              style={styles.input}
+              placeholder="Qual a matéria?"
+              placeholderTextColor="#c1bccc"
+              value={subject}
+              onChangeText={text => setSubject(text)}
+            />
+            
+            <View style={styles.inputGroup}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Dia da semana</Text>
+
+                <TextInput
+                  style={styles.input}
+                  placeholder="Qual o dia?"
+                  placeholderTextColor="#c1bccc"
+                  value={week_day}
+                  onChangeText={text => setWeekDay(text)}
+                />
+              </View>
+
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Horário</Text>
+
+                <TextInput
+                  style={styles.input}
+                  placeholder="Qual horário?"
+                  placeholderTextColor="#c1bccc"
+                  value={time}
+                  onChangeText={text => setTime(text)}
+                />
+              </View>
+            </View>
+
+            <RectButton 
+              style={styles.applyFiltersButton} 
+              onPress={handleApplyFilters}
+            >
+              <Text style={styles.applyFiltersButtonText}>Aplicar Filtros</Text>
+            </RectButton>
+          </View>
+        )}
+      </PageHeader>
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 12
+        }}
+      >
+        {teachers.map((teacher: Teacher) => {
+          return (
+            <TeacherItem 
+              key={teacher.id}
+              teacher={teacher}
+              favorited={favorites.includes(teacher.id)}
+            />
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
--- src/pages/TeacherList/styles.ts
+++ src/pages/TeacherList/styles.ts
@@ -1,9 +1,66 @@
 import { StyleSheet } from 'react-native';
+import { Poppins_400Regular } from '@expo-google-fonts/poppins';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f0f0f7'
+  },
+
+  filterButton: {
+    flexDirection: 'row'
+  },
+
+  filterButtonText: {
+    color: "#fff",
+    marginRight: 4
+  },
+
+  teacherList: {
+    marginTop: -40,
+  },
+
+  searchForm: {
+    marginBottom: 16
+  },
+
+  label: {
+    color: "#d4c2ff",
+    fontFamily: "Poppins_400Regular"
+  },
+
+  inputGroup: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+
+  inputBlock: {
+    width: "48%"
+  },
+
+  input: {
+    height: 54,
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    justifyContent: "center",
+    paddingHorizontal: 16,
+    marginTop: 4,
+    marginBottom: 18
+  },
+
+  applyFiltersButton: {
+    backgroundColor: "#04d361",
+    height: 56,
+    borderRadius: 8,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  applyFiltersButtonText: {
+    color: "#fff",
+    fontFamily: "Archivo_700Bold",
+    fontSize: 16,
   }
 });
 
